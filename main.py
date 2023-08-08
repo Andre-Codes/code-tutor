6ad48bb2f979e471de34dacc34f7046246edc4d9
Andre <64148828+Andre-Codes@users.noreply.github.com>
2023-08-08 10:33:14 -0500

    initial

---
+++ main.py
@@ -0,0 +1,14 @@
+import gpt_service as gpt
+
+system_role = "You're a helpful assistnat and expert on analyzing python library documentation"
+
+ai = gpt.GPTService(system_role=system_role, temperature=0.2)
+
+ai.prompt = "Summarize sklearn ordinal encoder, respond in JSON format with fields \
+for description, parameters, attributes, examples. Use markdown for headings and code"
+
+answer = ai.get_response(ai.prompt)
+
+print(answer)
+
+
