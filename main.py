a39583b89af50e3c70cf231301bc1c7bc624da83
Andre <64148828+Andre-Codes@users.noreply.github.com>
2023-09-01 08:04:25 -0500

    completed html response feature

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import gpt_service as gpt
 
+# Example using the 'api_explain' context and requiring
+# a response in HTML format
 test = gpt.GPTService(role_context='api_explain',
                             temperature=0, 
                             prompt_context=False,
                             comment_level='detailed',
                             explain_level='comprehensive')
 
-test.prompt = "pandas unstack function"
+test.prompt = "pandas to_numeric function"
 
 test.get_response(test.prompt, format_style='html')
 
 
-
-
-code_help = gpt.GPTService(role_context='code_help',
-                            temperature=0.2, comment_level='Exhaustive')
+# Example using the 'code_help' context
+code_help = gpt.GPTService(role_context='code_help', comment_level='exhaustive')
