ea2828b77c80535349837bcc3e41e2b20155eb40
Andre <64148828+Andre-Codes@users.noreply.github.com>
2023-10-24 12:46:33 -0500

    updated handle_response to return the prompt

--- web_app/code_tutor_web.py
+++ web_app/code_tutor_web.py
@@ -238,7 +238,7 @@ def handle_response(chat_engine,
 
         st.toast(':teacher: All replies ready!', icon='✅')
 
-        return response_1
+        return response_1, prompt
 
     except Exception as e:
         st.error(f"There was an error handling your question!\n\n{e}", icon='🚨')
@@ -267,10 +267,10 @@ def main():
             st.chat_message('user').markdown(message)
     # Initiate the OpenAI response upon button press
     if response_button:
-        response = handle_response(chat_engine, selected_friendly_role,
+        response, prompt = handle_response(chat_engine, selected_friendly_role,
                                    config_settings, extra_response_toggle,
                                    helper_prompt, prompt=chat_engine.user_prompt)
-        st.session_state[context]['messages'].extend([chat_engine.user_prompt, response])
+        st.session_state[context]['messages'].extend([prompt, response])
 
     # Store the prompt and the response in a session_state list
     # for use in chatting function
